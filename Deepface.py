62689e7903744c870c62c48367a04dfd90ac1c98
Ehtz <121399963+Ehtz@users.noreply.github.com>
2023-10-18 19:45:34 +0100

    Update Deepface.py
    Updated with new Drop Down and Cleaned UI

--- Deepface.py
+++ Deepface.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[79]:
-
-
 import tkinter as tk
+from tkinter import *
+from tkinter import font
 from tkinter import filedialog
+from tkinter import ttk 
 from deepface import DeepFace
 import os
-import matplotlib.pyplot as plt
-import cv2
 import pandas as pd
-from IPython.display import display
 
-# Initialize global variables
 db_path = ""
 img2_path = ""
 dfs = []  # Initialize dfs as an empty list
+models = [
+    "VGG-Face",
+    "Facenet",
+    "Facenet512",
+    "OpenFace",
+    "DeepFace",
+    "DeepID",
+    "ArcFace",
+    "Dlib",
+    "SFace",
+]
+
+
+
 
 # Function to open a folder dialog and update db_path
 def select_folder():
@@ -44,20 +54,7 @@ def open_second_image():
     else:
         result_text.delete(1.0, tk.END)
         result_text.insert(tk.END, "Please select a second image.")
-        
-def open_first_image_from_list():
-    global dfs
-    if dfs:
-        try:
-            # Convert the Series object to a string and then open the image
-            os.startfile(str(dfs[0]['identity']))
-        except Exception as e:
-            result_text.delete(1.0, tk.END)
-            result_text.insert(tk.END, f"Error opening the image: {str(e)}")
-    else:
-        result_text.delete(1.0, tk.END)
-        result_text.insert(tk.END, "No results available.")
-        
+
 # Function to perform the DeepFace.find() when the user clicks a button
 def find_similar_faces():
     global db_path, img2_path, dfs
@@ -66,65 +63,83 @@ def find_similar_faces():
         result_text.insert(tk.END, "Please select both a database folder and a second image.")
     else:
         pd.set_option('display.max_colwidth', -1)
-        dfs = DeepFace.find(img2_path, db_path=db_path, model_name='Facenet512', enforce_detection=False)
+        pd.options.display.max_rows = 4000
+        selected_model = model_selection.get()  # Get the selected model from the dropdown
+        dfs = DeepFace.find(img2_path, db_path=db_path, model_name=selected_model, enforce_detection=False)
 
         # Display the results in the text widget
         result_text.delete(1.0, tk.END)  # Clear previous results
-        for item in dfs:
-            result_text.insert(tk.END, f"Image Path: {item['identity']}\n")
-            result_text.insert(tk.END, f"Facenet512_cosine: {item['Facenet512_cosine']}\n\n")
-            
-        # Enable the "Open First Image from List" button
-        open_first_image_from_list_button.config(state=tk.DISABLED)
 
-# Initialize the tkinter GUI
+        for i, item in enumerate(dfs):
+            combined_text = f"{i + 1}. {item['identity']}  {selected_model}_cosine: {item[selected_model + '_cosine']}\n"
+            result_text.insert(tk.END, combined_text)
+
+            
+        
+# Create a main window
 root = tk.Tk()
-root.title("Folder and Image Selection")
+root.title("Simple DeepFace Gui by Ehtz")
+
 
 # Create a label and an entry widget for db_path
 db_path_label = tk.Label(root, text="Database Images Folder:")
-db_path_label.pack()
+db_path_label.grid(row=0, column=0)
+
+select_folder_button = tk.Button(root, text="Select Folder", command=select_folder)
+select_folder_button.grid(row=0, column=1)
 
 db_path_entry = tk.Entry(root, textvariable=db_path, width=50)
-db_path_entry.pack()
+db_path_entry.grid(row=0, column=2)
 
-# Create a button to open the folder selection dialog for db_path
-select_folder_button = tk.Button(root, text="Select Folder", command=select_folder)
-select_folder_button.pack()
+
+
+
 
 # Create a label and an entry widget for img2_path
 img2_path_label = tk.Label(root, text="Base Image:")
-img2_path_label.pack()
-
-img2_path_entry = tk.Entry(root, textvariable=img2_path, width=50)
-img2_path_entry.pack()
+img2_path_label.grid(row=1, column=0, pady=25)
 
 # Create a button to open the file selection dialog for img2_path
 select_image_button = tk.Button(root, text="Select Image", command=select_image)
-select_image_button.pack()
+select_image_button.grid(row=1, column=1)
 
 # Create a button to open the selected second image
 open_second_image_button = tk.Button(root, text="Open Base Image", command=open_second_image)
-open_second_image_button.pack()
+open_second_image_button.grid(row=1, column=3)
 
-# Create a button to open the picture of the first image from the dfs list
-open_first_image_from_list_button = tk.Button(root, text="Open First Image from List", command=open_first_image_from_list,state=tk.DISABLED)
-open_first_image_from_list_button.pack()
+img2_path_entry = tk.Entry(root, textvariable=img2_path, width=50)
+img2_path_entry.grid(row=1, column=2)
+
+
+
+
+
+# Create a dropdown menu for selecting the model
+model_label = tk.Label(root, text="Select Model:")
+model_label.grid(row=4, column=0)
+
+model_selection = ttk.Combobox(root, values=models)
+model_selection.set("Facenet512")
+model_selection.grid(row=4, column=1)
+
+
+
 
 # Create a button to trigger the find_similar_faces function
-find_button = tk.Button(root, text="Find Similar Faces", pady=15, bg="green", command=find_similar_faces)
-find_button.pack()
+find_button = tk.Button(root, text="Find Similar Faces", pady=15, bg="red", fg='white', command=find_similar_faces)
+find_button.grid(row=6, column=0, columnspan=5, padx=10, pady=10)
 
-# Create a text widget to display the results
-result_text = tk.Text(root, width=200, height=100)
-result_text.pack()
+# Create a text wido display the results
+result_text = tk.Text(root, width=200, height=25, selectbackground="yellow", selectforeground='black', undo=True)
+result_text.grid(row=7, column=0, columnspan=5, padx=10, pady=10)
+
+# Create a scrollbar and attach it to the text widget
+text_scroll = tk.Scrollbar(root, command=result_text.yview)
+text_scroll.grid(row=7, column=5, sticky="ns")
+result_text.config(yscrollcommand=text_scroll.set)
+
+
 
 # Start the tkinter main loop
+root.geometry("1650x500")
 root.mainloop()
-
-
-# In[ ]:
-
-
-
-
